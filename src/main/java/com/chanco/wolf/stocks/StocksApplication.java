da7b546945a94d2071f92486acbb2bc36f396c1e
seamus <guoyingxie@126.com>
2019-10-30 09:19:33 +0800

    update

---
+++ src/main/java/com/chanco/wolf/stocks/StocksApplication.java
@@ -0,0 +1,14 @@
+package com.chanco.wolf.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/chanco/wolf/stocks/StocksApplicationTests.java
@@ -0,0 +1,17 @@
+package com.chanco.wolf.stocks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StocksApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
